05161a566616b53d75c9c0e9b082d3a1a9d5192b
IdaStr <ida.v.s@hotmail.no>
2025-03-29 04:43:07 +0100

    Added quest mail box. Fully functional.
    Click F on the mailbox as much as you want to read the quest! Also added a slight Rig fix. You could see his body through his tshirt when running so I updated his rig. All animations work still, I checked.

--- Bachelor24_25/Assets/Scenes/Scripts/MailBoxQuest.cs
+++ Bachelor24_25/Assets/Scenes/Scripts/MailBoxQuest.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MailBoxQuest : MonoBehaviour
+
 {
+    public GameObject MailboxExclamation;
+    public GameObject InteractionKEY;
+    public GameObject QuestRequest;
+
+    
+    private bool hasInteracted;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +20,75 @@ public class MailBoxQuest : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+       
+        if(MailboxExclamation!= null) 
+        {
+            MailboxExclamation.transform.LookAt(Camera.main.transform.position);
+            MailboxExclamation.transform.Rotate(0, 180, 0);
+            //makes the ! in the scene follow and be rotated towards the camera so its always visible to player.
+        }
+
+        if (!hasInteracted)
+        {
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Interact();
+            }
+            if (Input.GetKeyDown(KeyCode.F)) 
+            {
+                InteractionKEY.SetActive(false);
+            }
+        }
+
     }
+
+    public void Interact() 
+    { 
+        if (!hasInteracted) 
+        { 
+          InteractionKEY.SetActive(true);
+            QuestRequest.SetActive(true);
+            MailboxExclamation.SetActive(false);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if (other.CompareTag("Player"))
+        {
+            InteractionKEY.SetActive(true);
+
+            //if (!hasInteracted)
+           // {
+              //  MailboxExclamation.SetActive(true);
+            //}
+           
+           // else if (!hasInteracted)
+           // {
+             //   MailboxExclamation.SetActive(false);
+          //  }
+
+
+            Debug.Log("Player has entered zone.");
+        }
+
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && hasInteracted)
+        {
+    
+            MailboxExclamation.SetActive(true);
+           
+        }
+
+        Debug.Log("Player has fucked off out of zone.");
+    }
+
 }
