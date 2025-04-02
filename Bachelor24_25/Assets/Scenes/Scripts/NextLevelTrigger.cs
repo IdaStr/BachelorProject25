a36c0ed4fb2b7d4b9de2a78e7ce08429c8a3397c
IdaStr <ida.v.s@hotmail.no>
2025-04-02 02:39:06 +0200

    Added trigger zone for ending game/going to next level
    Just need to add the various button options for a fun lil easteregg at the end.

---
+++ Bachelor24_25/Assets/Scenes/Scripts/NextLevelTrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class NextLevelTrigger : MonoBehaviour
+{
+    public GameObject NextLevelEndCredits;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        {   if (other.tag == "Player")
+
+                Debug.Log("Player has entered next level zone");
+
+        }
+
+        {
+            NextLevelEndCredits.SetActive(true);
+
+        }
+    }
+}
---
+++ Bachelor24_25/Assets/Scenes/Scripts/SceneLoaderQuit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoaderQuit : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit the game!");
+
+    }
+}
