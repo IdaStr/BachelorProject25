b3ecbc675917ae620ac61798e635af757c5c0e85
MarenSofie <margau30932@stud.noroff.no>
2025-04-12 14:02:35 +0200

    playmenu works wohoo

---
+++ Bachelor24_25/Assets/Scenes/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
